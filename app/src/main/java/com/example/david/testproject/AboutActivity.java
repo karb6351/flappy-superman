c1e73ccc907cd5a7d6ddc43f15bba4538d2b120f
Ho wai chi <karb6351@gmail.com>
2018-11-24 18:32:30 +0800

    finsh about activities

--- app/src/main/java/com/example/david/testproject/AboutActivity.java
+++ app/src/main/java/com/example/david/testproject/AboutActivity.java
@@ -2,9 +2,13 @@ package com.example.david.testproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 public class AboutActivity extends AppCompatActivity {
+
+    private Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +19,12 @@ public class AboutActivity extends AppCompatActivity {
             getSupportActionBar().hide();
         }
         setContentView(R.layout.activity_about);
+        backButton = findViewById(R.id.about_back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
